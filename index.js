e245fff36c3717356b6c00227a878c8dc32c8306
edmond <edmond.wanyama@student.moringaschool.com>
2023-06-16 16:17:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var recipes = {}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return employee.assign(employee, {[key]: value})
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+  return employee
+}
+
+function deleteFromObjectByKey(employee, key) {
+  var newemployee = employee.assign({}, employee)
+  delete employee[key]
+  return employee
+}
+
+function destructivelyDeleteFromemployeeByKey(employee, key) {
+  delete employee[key]
+  return employee
+}
